00bab061d99cfc5fa9ca92848d521919333e60bc
BP-36-201-01 <bp-36-201-01@BP-36-201-01s-iMac.local>
2025-11-23 09:25:05 +0300

    initial

---
+++ Fixify/Fixify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fixify
+//
+//  Created by BP-36-201-01 on 23/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
