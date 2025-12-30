8324e10b16d1160c7236f674cbed9e419be804ae
SalmanAlnaar <202303491@student.polytechnic.bh>
2025-12-30 18:42:18 +0300

    added the manage technician functionality as well as adding a new technician

---
+++ Fixify/Fixify/Admin/Modules/TechnicianManagement/Controller/ManageTechnicianViewController.swift
@@ -0,0 +1,121 @@
+import UIKit
+
+final class ManageTechnicianViewController: UIViewController {
+
+    private let viewModel: ManageTechnicianViewModel
+    private let tableView = UITableView()
+
+    init(requestID: String) {
+        self.viewModel = ManageTechnicianViewModel(requestID: requestID)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) not implemented")
+    }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Assign Technician"
+        view.backgroundColor = .systemGroupedBackground
+
+        setupTableView()
+
+        // ✅ Bind reload callback
+        viewModel.onUpdate = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+
+        // ✅ Load data
+        viewModel.load()
+    }
+
+    // MARK: - Table Setup
+
+    private func setupTableView() {
+
+        tableView.register(
+            TechnicianCell.self,
+            forCellReuseIdentifier: TechnicianCell.identifier
+        )
+
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    // MARK: - Error
+
+    private func showError(_ msg: String) {
+        let alert = UIAlertController(
+            title: "Error",
+            message: msg,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ManageTechnicianViewController: UITableViewDataSource {
+
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        viewModel.technicians.count
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: TechnicianCell.identifier,
+            for: indexPath
+        ) as! TechnicianCell
+
+        let technician = viewModel.technician(at: indexPath.row)
+
+        cell.configure(
+            with: technician,
+            isCurrentlyAssigned: viewModel.isCurrentlyAssigned(technician)
+        )
+
+        cell.onAssignTapped = { [weak self] in
+            guard let self else { return }
+
+            self.viewModel.assignTechnician(technician) { success in
+                DispatchQueue.main.async {
+                    if success {
+                        self.navigationController?.popViewController(animated: true)
+                    } else {
+                        self.showError(
+                            "Technician could not be assigned. Request may be locked."
+                        )
+                    }
+                }
+            }
+        }
+
+        return cell
+    }
+}
